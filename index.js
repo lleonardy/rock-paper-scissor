7e198b6011e18605ec5eed820538bca31956fa6e
Wicaksono leonardy <wicaksonoleonardy@gmail.com>
2025-12-14 03:08:44 +0700

    reworked the game mechanic to use buttons and having user interface

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ console.log("Rock-Paper-Scissor! ");
 let humanScore = 0;
 let computerScore = 0;
 
+const scoreDisplay = document.querySelector("#score");
+const messageDisplay = document.querySelector("#message");
+const buttons = document.querySelectorAll("button");
+const btnRestart = document.querySelector("#btnRestart");
+
 function getComputerChoice() 
 {
     const randomNumber = Math.random();
@@ -14,83 +19,81 @@ function getComputerChoice()
     }else {
         return "scissor";
     }
-    
 }
 
-function getHumanChoice() 
+function playRound(humanChoice) 
 {
-    const playerSelection = prompt("Enter your choice: [ rock | paper | scissor |");
+    if (humanScore >= 5 || computerScore >= 5) return;
 
-    if (playerSelection === null){
-        return null;
+    const computerChoice = getComputerChoice();
+
+    if(humanChoice === computerChoice) {
+        messageDisplay.textContent = `Tie! you both chose ${humanChoice}`;
     }
-
-    return playerSelection.toLowerCase();
-}
-
-function playRound(humanChoice, computerChoice) {
-    
-    const playerChoice = humanChoice;
-
-    let result;
-
-    if (playerChoice === computerChoice) {
-        console.log(`Its a Tie! Both played ${playerChoice}`);
-        result = "tie";
-    } else if ( 
-                (playerChoice === "rock" && computerChoice === "scissor") ||
-                (playerChoice === "paper" && computerChoice === "rock") ||
-                (playerChoice === "scissor" && computerChoice === "paper")
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissor") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissor" && computerChoice === "paper")
     ) {
-        console.log(`You win! ${playerChoice} beats ${computerChoice}!`);
-
         humanScore++;
-        result = "win";
-    } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
-
+        messageDisplay.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+    }
+    else {
         computerScore++;
-        result = "lose";
+        messageDisplay.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
     }
 
-    return result;
+    scoreDisplay.textContent = `Player: ${humanScore} - Computer: ${computerScore}`;
+    checkWinner();
 
 }
 
-function playGame() {
-
-    const rounds = 5;
-
-    console.log("Starting 5 round rock-paper-scissor game..");
-    console.log("Input your choice when prompted: ");
-
-    for (let i=1; i <= rounds; i++) {
-        console.log(`Round ${i} of ${rounds} `);
-
-        const playerSelection = getHumanChoice();
-
-        if (playerSelection === null) {
-            console.log("Game cancelled");
-            return;
-        }
-
-        const computerSelection = getComputerChoice();
-
-        playRound(playerSelection, computerSelection);
-
-        console.log(`Current Score: Player ${humanScore} | Computer ${computerScore} `);
-
-    }
-
-    console.log("Game over")
-    if (humanScore > computerScore) {
-        console.log(`You win! Final score: ${humanScore} to ${computerScore} `);
-    } else if (humanScore < computerScore) {
-        console.log(`You lose Final score: ${humanScore} to ${computerScore} `);
-    } else {
-        console.log(`The game ends in a TIE`);
+function checkWinner() {
+    if (humanScore === 5) {
+        messageDisplay.textContent = `You win the game! you reached 5 points`;
+        messageDisplay.style.color = `green`;
+        endGame();
+    } else if (computerScore === 5) {
+        messageDisplay.textContent = `You lose! computer reached 5 points`;
+        messageDisplay.style.color = "red";
+        endGame();
     }
 }
 
-playGame();
+function endGame() {
+    buttons.forEach(btn => btn.disabled = true);
+    btnRestart.style.display = `block`;
+
+    btnRestart.disabled = false;
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    scoreDisplay.textContent = "Player: 0 - Computer: 0";
+    messageDisplay.textContent = "";
+    messageDisplay.style.color = "black";
+
+    buttons.forEach(btn => btn.disabled = false);
+
+    btnRestart.style.display = `none`;
+}
+
+document.querySelector("#btnRock").addEventListener("click", () => {
+    playRound("rock");
+});
+
+document.querySelector("#btnPaper").addEventListener("click", () => {
+    playRound("paper");
+});
+
+document.querySelector("#btnScissor").addEventListener("click", () => {
+    playRound("scissor");
+});
+
+document.querySelector("#btnRestart").addEventListener("click", () => {
+    resetGame();
+});
+
 
